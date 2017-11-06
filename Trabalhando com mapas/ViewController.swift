a41703f1e28e4268f201ca493b6d37d701d87794
Rodrigo Abreu <rodrigoabreu@rodrigos-macbook-pro.local>
2017-11-06 14:59:48 -0200

    app mapa

--- Trabalhando com mapas/ViewController.swift
+++ Trabalhando com mapas/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    
+    @IBOutlet var mapa: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+ 
+        let latitude: CLLocationDegrees = -23.586974
+        let longetude: CLLocationDegrees = -46.657355
+        
+        let deltaLatitude: CLLocationDegrees = 0.01
+        let deltaLongetude: CLLocationDegrees = 0.01
+        
+        //configurar coordenadas 2D
+        let localizacao: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longetude)
+        
+        //configurar o zoom
+        let areaVisualizacao: MKCoordinateSpan = MKCoordinateSpanMake(deltaLatitude, deltaLongetude)
+        
+        //configurar uma regiao para o mapa
+        let regiao: MKCoordinateRegion = MKCoordinateRegionMake(localizacao, areaVisualizacao)
+        
+        //exibir regiao no mapa
+        mapa.setRegion(regiao, animated: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
